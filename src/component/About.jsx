0f353063b280295e18f34f841107b1f50508b8ec
Sala 310 <sala310@poliacademico.edu.loc>
2019-07-17 12:49:32 -0500

    React

---
+++ src/component/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//const About = function (){
+   // return <h1>Hola, mundo GEEK</h1>}
+
+//const AboutDos = () => {
+    //return <h1>Hola mundo GEEK</h1>
+//}
+//retornar de manera explícita
+const About = () => (<h1>Hola mundo GEEK</h1>);
+
+export default About;
